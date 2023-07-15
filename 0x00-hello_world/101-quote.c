205aa6130143452b213b9429de986a13279d7ded
Phrank08 <frankokey469@.com>
2023-07-15 11:10:49 -0700

    updated errors in files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- *main .A c program that print a line to the standard error
- *Return 1 (Success)
+ * main - Entry point of the program
+ *
+ * Description: Prints the desired message to the standard error.
+ *
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
-
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r , stdou );
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
+
