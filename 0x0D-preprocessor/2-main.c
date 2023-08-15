cce20188ab61b5646cd9f0846ac8bd0e22753e4c
Phrank08 <frankokey469@gmail.com>
2023-08-15 13:55:49 -0700

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * description - A program that prints the source code file
+ *
+ * Author - Eneh Franklyn Okechukwu
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n",__FILE__);
+
+	return (0);
+}
