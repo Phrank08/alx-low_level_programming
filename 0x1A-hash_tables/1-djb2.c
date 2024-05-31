8c8020cc5a9f879614d3286fd2b0989831bf0d51
Phrank08 <frankokey469@gmail.com>
2024-05-30 18:33:13 -0700

    a hash function implementing the djb2 algorithm.

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,6 +1,7 @@
 #include "hash_tables.h"
+
 /**
- * hash_djb2 - implementation of the djb2 algorithm
+ * hash_djb2 - Implementation of the djb2 algorithm
  * @str: string used to generate hash value
  *
  * Return: hash value
