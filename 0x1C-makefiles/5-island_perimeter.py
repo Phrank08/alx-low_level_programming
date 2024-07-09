5ba770b21e0223d3137e678056937e58e8c7f3f0
Phrank08 <frankokey469@gmail.com>
2024-07-09 01:10:30 -0700

    Done

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
-"""Module returns perimater of island described
-in Grid"""
+""" periemter module """
 
 
 def island_perimeter(grid):
-    """Method returns perimeter of island described
-    in Grid"""
-    n = 0
-    for i, row in enumerate(grid):
-        for j, element in enumerate(row):
-            if element == 1:
-                n += 1
-            if j != len(row):
-                if grid[i][j + 1] == 1
-    perimeter = n * 2 + 2
-    print(i)
-    print(j)
-    return perimeter
+    """ method that returns the perimeter of the island
+    described in grid
+    Args:
+        grid(list): is a list of integers
+        where 1 represents land zone and 0 water
+    grid is rectancular and width and height don't exceed the 100
+    grid is completely sorrounded by water and there is one island
+    or nothing.
+    one cell of the grid is a square with side lenght 1
+    """
+    lands = 0
+    side = 0
+    for height in range(len(grid)):
+        for width in range(len(grid[height])):
+            if grid[height][width] == 1:
+                lands += 1
+                if height > 0 and grid[height-1][width] == 1:
+                    side += 1
+                if width > 0 and grid[height][width-1] == 1:
+                    side += 1
+    return lands * 4 - side * 2
